0a3a409ced54b7f09d972c15dfd30a50cc58a73b
Lucas M. Bonomo <lucas@vgnet.com.ar>
2020-11-11 10:40:14 -0300

    List styles

---
+++ src/components/collections/list/style.js
@@ -0,0 +1,21 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  container: {
+    marginTop: '3rem',
+    marginBottom: '2rem',
+    width: '100%',
+  },
+
+  dataGridContainer: {
+    height: 400,
+    width: '100%',
+    margin: '1rem 0',
+  },
+  cardItem: {
+    padding: '0.5rem',
+  },
+
+}))
+
+export default useStyles
